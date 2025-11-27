983b1677f5bb71334554b7eab9318e6645629bd0
lizzardcz <lizzardcz@noreply.gitcode.com>
2025-11-27 22:10:14 +0800

    Initial Commit

---
+++ FightPet Watch App/FightPetApp.swift
@@ -0,0 +1,17 @@
+//
+//  FightPetApp.swift
+//  FightPet Watch App
+//
+//  Created by lizzardchen on 11/27/25.
+//
+
+import SwiftUI
+
+@main
+struct FightPet_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
